32ff099818e3d5aedf92474ad0ddcfc1f6916d77
Clay Diffrient <cdiffrient@instructure.com>
2015-11-03 23:41:35 -0700

    Set up babel and webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+let x = 123;
+
+console.log(x);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: __dirname + '/built/',
+    filename: 'bundle.js',
+    publicPath: '/built/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/ }
+    ]
+  }
+};
