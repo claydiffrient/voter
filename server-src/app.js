775f61541523e796a5c033cbfff120c243da22aa
Clay Diffrient <cdiffrient@instructure.com>
2015-11-19 01:06:20 -0700

    Add passport to authenticate via JWT

--- server-src/app.js
+++ server-src/app.js
@@ -11,13 +11,21 @@ import http from 'http';
 import config from 'config';
 import r from 'rethinkdb';
 import renderApp from './ssrApp';
-
+import passport from 'passport';
+import { Strategy } from 'passport-local';
+import User from './models/User';
+import jwt from 'express-jwt';
 
 // import apiRoutes from './api'
 
 const app = express();
 const httpServer = http.Server(app);
 const port = 3000;
+
+const auth = jwt({
+  secret: config.get('jwt.secret'),
+  userProperty: 'payload'
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -27,6 +35,28 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
+const LocalStrategy = Strategy;
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+  },
+  (email, password, done) => {
+    User
+      .filter({ email })
+      .run()
+      .then((result) => {
+        if (!result.length) {
+          return done(null, false, { message: 'Incorrect email'});
+        }
+        if (!result[0].validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password'});
+        }
+        return done(null, result[0]);
+      });
+  }
+));
+
+app.use(passport.initialize());
 
 // app.get('/', renderApp);
 app.get('/', (req, res) => {
@@ -46,7 +76,7 @@ app.get('/api/v1/items', (req, res) => {
    });
 });
 
-app.post('/api/v1/items', (req, res) => {
+app.post('/api/v1/items', auth, (req, res) => {
   r.connect(config.get('db'))
    .then((conn) => {
      return r.table('items')
@@ -74,6 +104,38 @@ app.post('/api/v1/items/:id/vote', (req, res) => {
                res.json(response.changes[0].new_val);
              });
    });
+});
+
+app.post('/register', (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({message: 'You must provide an email and a password.'});
+  }
+
+  var user = new User({
+    email: req.body.email
+  });
+  user.setPassword(req.body.password);
+
+  user.save((err) => {
+    if (err) { return next(err); }
+    return res.json({token: user.generateJWT()});
+  });
+});
+
+app.post('/login', (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({message: 'You must provide an email and a password.'});
+  }
+
+  passport.authenticate('local', (err, user, info) => {
+    if (err) { return next(err); }
+
+    if (user) {
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 httpServer.listen(port);
