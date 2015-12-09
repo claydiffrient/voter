3a179ca497a7fbbc20925e194eb111831e7ceec2
Clay Diffrient <cdiffrient@instructure.com>
2015-12-08 23:13:14 -0700

    fix missing it statement for util spec

--- test/utils_spec.js
+++ test/utils_spec.js
@@ -3,11 +3,11 @@ import { expect } from 'chai';
 import * as Utils from '../src/utils';
 
 describe('Utils Specs', () => {
-
   describe('setToken', () => {
-    let expected = {token: 'test'};
-    Utils.setToken(expected);
-    expect(Utils.getToken()).to.deep.equal(JSON.stringify(expected));
+    it('sets the token appropriately', () => {
+      let expected = {token: 'test'};
+      Utils.setToken(expected);
+      expect(Utils.getToken()).to.deep.equal(JSON.stringify(expected));
+    });
   });
-
 });
