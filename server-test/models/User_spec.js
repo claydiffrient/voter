34429064051bb945c2a8a7e6fdffff8f4344c25e
Clay Diffrient <cdiffrient@instructure.com>
2015-12-29 23:41:00 -0600

    Go back to built tests
    This adds a few more tests as well.  I'm still working on the code coverage portions, but
    this should get things closer.

--- server-test/models/User_spec.js
+++ server-test/models/User_spec.js
@@ -2,37 +2,55 @@
 import { expect } from 'chai';
 import thinkyConstructor from 'thinky';
 import config from 'config';
-import UserModelFactory from '../../server-src/models/User';
+import UserModelFactory from '../../server/models/User';
 
 describe('User Model', () => {
   let thinky;
   let UserModel;
+  let baseUser;
 
   before(() => {
     thinky = thinkyConstructor(config.get('db'));
+    UserModel = UserModelFactory(thinky);
   });
 
   beforeEach(() => {
-    UserModel = UserModelFactory(thinky);
+    baseUser = new UserModel({
+      email: 'test@example.com',
+      name: 'Tester McTesty',
+      username: 'testerman'
+    });
+    baseUser.setPassword('sekrit');
+  });
+
+  afterEach(() => {
+    baseUser = null;
   });
 
   describe('#toPublic', () => {
     it('removes the hash and salt properties', () => {
-      const user = new UserModel({
-        email: 'test@example.com',
-        name: 'Tester McTesty',
-        username: 'testerman'
-      });
-      user.setPassword('sekrit');
 
-      expect(user.hash).to.be.ok;
-      expect(user.salt).to.be.ok;
+      expect(baseUser.hash).to.be.ok;
+      expect(baseUser.salt).to.be.ok;
 
-      const sanitized = user.toPublic();
+      const sanitized = baseUser.toPublic();
 
       expect(sanitized.hash).to.not.be.ok;
       expect(sanitized.salt).to.not.be.ok;
 
+    });
+  });
+
+  describe('#validPassword', () => {
+
+    it('returns true when the password is valid', () => {
+      const actual = baseUser.validPassword('sekrit');
+      expect(actual).to.be.ok;
+    });
+
+    it('returns false when the password is not valid', () => {
+      const actual = baseUser.validPassword('secret');
+      expect(actual).to.not.be.ok;
     });
   });
 });
