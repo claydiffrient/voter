0dc3fad5b1f07f45e999c920d382b97c0071f545
Clay Diffrient <cdiffrient@instructure.com>
2015-12-30 23:30:32 -0700

    spec: Add test for generateJWT method

--- server-test/models/User_spec.js
+++ server-test/models/User_spec.js
@@ -1,5 +1,7 @@
 /*eslint-env mocha */
 import { expect } from 'chai';
+import atob from 'atob';
+import tk from 'timekeeper';
 import thinkyConstructor from 'thinky';
 import config from 'config';
 import UserModelFactory from '../../server/models/User';
@@ -51,6 +53,35 @@ describe('User Model', () => {
     it('returns false when the password is not valid', () => {
       const actual = baseUser.validPassword('secret');
       expect(actual).to.not.be.ok;
+    });
+  });
+
+  describe('#generateJWT', () => {
+    const TIME = 1451542611955;
+    before(() => {
+      tk.freeze(TIME);
+    });
+
+    after(() => {
+      tk.reset();
+    });
+
+    const parseJWT = (token) => {
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace('-', '+').replace('_', '/');
+      return JSON.parse(atob(base64));
+    };
+
+    it('generates a valid JWT', () => {
+      const jwt = baseUser.generateJWT();
+      const parsed = parseJWT(jwt);
+
+      let expectedTime = new Date(TIME);
+      expectedTime.setDate(expectedTime.getDate() + 60);
+      expectedTime = parseInt(expectedTime.getTime() / 1000, 10);
+
+      expect(parsed.username).to.equal(baseUser.username);
+      expect(parsed.exp).to.equal(expectedTime);
     });
   });
 });
