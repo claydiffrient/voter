85eebdcaeab71b9a0749d0f32a6e086389c19f39
Clay Diffrient <cdiffrient@instructure.com>
2016-01-01 20:00:41 -0700

    Add additional tests around the User API

--- test/utils_spec.js
+++ test/utils_spec.js
@@ -16,7 +16,7 @@ describe('Utils Specs', () => {
     it('parses the JWT as expected', function () {
       const testJWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImNsYXlkaWZmcmllbnQiLCJleHAiOjE0NTU1OTI4ODMsImlhdCI6MTQ1MDQwODg4M30.eTXtoselT6BzI7X2BbIvjy4H4KGqVYUGCLetZYz4M2Y';
       const parsed = Utils.parseJWT(testJWT);
-      const expected = {username: 'claydiffrient', exp: 1455592883, iat: 1450408883}
+      const expected = {username: 'claydiffrient', exp: 1455592883, iat: 1450408883};
       expect(parsed).to.deep.equal(expected);
     });
   });
